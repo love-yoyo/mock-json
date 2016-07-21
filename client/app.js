04294a9af677fcb418cc08184d4191b1706bbaf2
King <yao_jin90@163.com>
2016-07-21 23:17:43 +0800

    refact with koa and angular

---
+++ client/app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('App', [
+    'ui.router',
+    'app.table'
+]);
---
+++ client/table/table.module.js
@@ -0,0 +1,27 @@
+/**
+ * @author v.lugovsky
+ * created on 16.12.2015
+ */
+(function() {
+    'use strict';
+
+    angular.module('app.table', [])
+        .config([
+            '$stateProvider', '$urlRouterProvider',
+            function($stateProvider, $urlRouterProvider) {
+                $stateProvider
+                    .state('table', {
+                        url: '/table',
+                        template: '<ui-view></ui-view>',
+                        abstract: true,
+                        controller: 'TablePageCtrl',
+                        title: 'Table'
+                    }).state('table.smart', {
+                        url: '/smart',
+                        templateUrl: 'table/table.html',
+                        title: 'Smart Tables'
+                    });
+                $urlRouterProvider.otherwise('/', '/table/smart');
+            }
+        ]);
+})();
